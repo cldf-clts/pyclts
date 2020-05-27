76c581d12376bb6ce917e39866a08814421cf476
xrotwang <xrotwang@googlemail.com>
2020-05-27 10:31:58 +0200

    release 2.1.1

--- tests/test_ipachart.py
+++ tests/test_ipachart.py
@@ -3,8 +3,10 @@ from pyclts.ipachart import *
 
 def test_ipa_charts():
     inventory = [
-        Segment('x', 'voiced alveolar nasal consonant', href='http://example.org'),
-        Segment('a', 'rounded close back vowel', href='http://example.org')
+        Segment('x', 'voiced alveolar nasal consonant', href='http://example.org', css_class='abc'),
+        Segment('a', 'rounded close back vowel', href='http://example.org', css_class='abc')
     ]
     html, covered = ipa_charts(inventory)
     assert covered == {0, 1}
+    html, covered = ipa_charts(inventory, colorspec={'abc': ('red', 'solid 1px green')})
+    assert 'abc' in html and 'green' in html
