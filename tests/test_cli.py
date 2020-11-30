4fa40a176d4f9f9be17801eea4316d69e47960c2
lingulist <mattis.list@lingpy.org>
2020-11-30 13:29:39 +0100

    update cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -40,7 +40,7 @@ def test_map(tmp_repos, capsys, fixtures):
 
 def test_make_dataset(tmp_repos, capsys, fixtures):
     main(['--repos', str(tmp_repos), 'make_dataset', 'allenbai'])
-
+    out, _ = capsys.readouterr()
 
 
 
