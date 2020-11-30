962ff66433e4a44646482a24c071b1de09e840f3
lingulist <mattis.list@lingpy.org>
2020-11-30 15:46:16 +0100

    first step towards refining inventories

--- src/pyclts/inventories.py
+++ src/pyclts/inventories.py
@@ -2,9 +2,11 @@
 Module handles different aspects of inventory comparison.
 """
 import attr
-from collections import defaultdict
+from collections import defaultdict, OrderedDict, namedtuple
 from pyclts.api import CLTS
 import statistics
+from pyclts.models import is_valid_sound
+from pyclts.cli_util import Table
 
 
 def reduce_features(sound, clts=None, features=None):
@@ -29,42 +31,102 @@ def reduce_features(sound, clts=None, features=None):
 
 
 @attr.s
+class Phoneme:
+    """
+    Base class for handling sounds.
+    """
+    grapheme = attr.ib(default=None)
+    grapheme_in_source = attr.ib(default=None, repr=False)
+    name = attr.ib(default=None)
+    type = attr.ib(default=None, repr=False)
+    occs = attr.ib(default=None, repr=False)
+    attributes = attr.ib(default=None)
+
+    def __len__(self):
+        return len(self.occs)
+
+    def __str__(self):
+        return self.grapheme
+
+
+@attr.s
 class Inventory:
-    sounds = attr.ib()
-    clts = attr.ib(default=None)
+    id = attr.ib(default=None)
+    sounds = attr.ib(default=None, repr=False)
+    unknown = attr.ib(default=None, repr=False)
+    language = attr.ib(default=None, repr=False)
+    bipa = attr.ib(default=None, repr=False)
 
     @classmethod
-    def from_list(cls, *list_of_sounds, clts=None):
-        clts = clts or CLTS().bipa
-
-        sounds = defaultdict(dict)
+    def from_list(cls, *list_of_sounds, language=None, bipa=None):
+        bipa = bipa or CLTS().bipa
+        unknown = []
+        sounds = OrderedDict()
         for itm in list_of_sounds:
-            sound = clts[itm]
-            sounds[sound.type][sound.s] = sound
-            for ft, val in sound.featuredict.items():
-                if val:
-                    sounds[val][sound.s] = sound
-        return cls(sounds=sounds, clts=clts)
+            sound = bipa[itm]
+            if sound.type in ['unknownsound', 'marker']:
+                unknown[str(sound)] = sound
+            else:
+                sounds[str(sound)] = Phoneme(
+                        grapheme=str(sound),
+                        grapheme_in_source=sound.grapheme,
+                        name=sound.name,
+                        type=sound.type,
+                        occs=[],
+                        attributes=sound)
+        return cls(sounds=sounds, bipa=bipa, unknown=unknown, language=language)
+
+    def __len__(self):
+        return len(self.sounds)
+
+    @property
+    def consonants(self):
+        return [s for s in self.sounds.values() if s.type=='consonant']
+
+    @property
+    def vowels(self):
+        return [s for s in self.sounds.values() if s.type=='vowel']
+
+    @property
+    def diphthongs(self):
+        return [s for s in self.sounds.values() if s.type=='diphthong']
+
+    @property
+    def clusters(self):
+        return [s for s in self.sounds.values() if s.type=='clusters']
+
+    @property
+    def tones(self):
+        return [s for s in self.sounds.values() if s.type=='tone']
+
+    def tabulate(self, format='pipe'):
+        table = []
+        for sound in self.sounds.values():
+            table += [[sound.grapheme, sound.type, sound.name, len(sound)]]
+        with Table(namedtuple('args', 'format')(format), 'Grapheme', 'Type', 'Name', 'Frequency') as table_text:
+            table_text += table
 
     def similar(self, other, metric="strict", aspects=None):
-        all_aspects = ["consonant", "vowel", "tone"]
+        all_aspects = ["consonants", "vowels", "tones"]
 
         def jac(a, b):
             return len(set(a).intersection(set(b))) / len(set(a).union(set(b)))
 
         if metric == "strict":
             if not aspects:
-                soundsA, soundsB = {}, {}
-                for aspect in all_aspects:
-                    soundsA.update(self.sounds[aspect])
-                    soundsB.update(other.sounds[aspect])
-
+                soundsA, soundsB = (
+                        {str(sound) for sound in self.sounds},
+                        {str(sound) for sound in other.sounds}
+                        )
                 score = jac(soundsA, soundsB)
             else:
                 scores = []
                 for aspect in aspects:
-                    if aspect in self.sounds or aspect in other.sounds:
-                        scores += [jac(self.sounds[aspect], other.sounds[aspect])]
+                    soundsA, soundsB = (
+                            {str(sound) for sound in getattr(self, aspect)},
+                            {str(sound) for sound in getattr(other, aspect)}
+                            )
+                    scores += [jac(soundsA, soundsB)]
 
                 if not scores:
                     score = 0.0
