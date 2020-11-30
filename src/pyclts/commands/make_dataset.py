11e78c7daf4368c4acbdb9c7395aec41e3197814
lingulist <mattis.list@lingpy.org>
2020-11-30 12:46:15 +0100

    update dataset

--- src/pyclts/commands/make_dataset.py
+++ src/pyclts/commands/make_dataset.py
@@ -17,6 +17,56 @@ except ImportError:
 from pyclts.models import is_valid_sound
 
 
+def process_transcription_data(rows, columns, src, uritemplate, bipa, args):
+    out = [[
+        'BIPA_GRAPHEME', 'CLTS_NAME', 'GENERATED', 'EXPLICIT', 'GRAPHEME',
+        'SYMBOLS', 'URL'] + columns]
+    graphemes = set()
+    for row in rows:
+        if row['GRAPHEME'] in graphemes:
+            args.log.warn('skipping duplicate grapheme: {0}'.format(row['GRAPHEME']))
+            continue
+        graphemes.add(row['GRAPHEME'])
+        if not row['BIPA']:
+            bipa_sound = bipa[row['GRAPHEME']]
+            explicit = ''
+        else:
+            bipa_sound = bipa[row['BIPA']]
+            explicit = '+'
+        generated = '+' if bipa_sound.generated else ''
+        if not explicit:
+            if is_valid_sound(bipa_sound, bipa):
+                bipa_grapheme = bipa_sound.s
+                bipa_name = bipa_sound.name
+                bipa_symbols = bipa_sound.symbols
+            else:
+                bipa_grapheme, bipa_name, bipa_symbols = 3 * ['<NA>']
+        else:
+            if row['BIPA'] == '<NA>':
+                bipa_grapheme, bipa_name, bipa_symbols = 3 * ['<NA>']
+            elif bipa_sound.type == 'unknownsound':
+                raise ValueError('wrong BIPA sound «{0}» in mapping'.format(
+                    row['BIPA']))
+            elif bipa_sound.type != 'marker' and not is_valid_sound(
+                    bipa_sound,
+                    bipa):
+                raise ValueError('invalid BIPA sound «{0}» in mapping'.format(
+                    row['BIPA']))
+            elif bipa_sound.type == 'marker':
+                bipa_grapheme, bipa_name, bipa_symbols = str(bipa_sound), '', ''
+            else:
+                bipa_grapheme, bipa_name, bipa_symbols = (
+                        str(bipa_sound), bipa_sound.name, bipa_sound.symbols)
+        url = uritemplate.expand(**row) if uritemplate else row.get('URL', '')
+        out.append(
+            [
+                bipa_grapheme, bipa_name, generated, explicit, row['GRAPHEME'],
+                bipa_symbols,
+                url] + [
+                    row.get(c, '') for c in columns])
+    return out
+
+
 def register(parser):
     parser.add_argument("dataset", help="the dataset")
 
@@ -33,30 +83,9 @@ def run(args):
     rows = args.repos.get_source(args.dataset)
     src = [src for src in args.repos.meta if src['NAME'] == args.dataset][0]
     uritemplate = URITemplate(src['URITEMPLATE']) if src['URITEMPLATE'] else None
-    out = [['BIPA_GRAPHEME', 'CLTS_NAME', 'GENERATED', 'EXPLICIT', 'GRAPHEME', 'URL'] + columns]
-    graphemes = set()
-    for row in rows:
-        if row['GRAPHEME'] in graphemes:
-            args.log.warn('skipping duplicate grapheme: {0}'.format(row['GRAPHEME']))
-            continue
-        graphemes.add(row['GRAPHEME'])
-        if not row['BIPA']:
-            bipa_sound = bipa[row['GRAPHEME']]
-            explicit = ''
-        else:
-            bipa_sound = bipa[row['BIPA']]
-            explicit = '+'
-        generated = '+' if bipa_sound.generated else ''
-        if is_valid_sound(bipa_sound, bipa):
-            bipa_grapheme = bipa_sound.s
-            bipa_name = bipa_sound.name
-        else:
-            bipa_grapheme, bipa_name = '<NA>', '<NA>'
-        url = uritemplate.expand(**row) if uritemplate else row.get('URL', '')
-        out.append(
-            [bipa_grapheme, bipa_name, generated, explicit, row['GRAPHEME'],
-             url] + [
-                row.get(c, '') for c in columns])
+    out = process_transcription_data(
+            rows, columns, src, uritemplate, bipa,
+            args)
     found = len([o for o in out if o[0] != '<NA>'])
     args.log.info('... {0} of {1} graphemes found ({2:.0f}%)'.format(
         found, len(out), found / len(out) * 100))
---
+++ src/pyclts/commands/test_dataset.py
@@ -0,0 +1,77 @@
+"""
+Prepare transcriptiondata from the transcription sources.
+"""
+from uritemplate import URITemplate
+from clldutils.clilib import ParserError
+from csvw.dsv import UnicodeWriter
+
+try:
+    from lingpy.sequence.sound_classes import token2class
+    from lingpy.data import Model
+    LINGPY = True
+except ImportError:
+    LINGPY = False
+    token2class = None
+    Model = None
+
+from pyclts.soundclasses import SOUNDCLASS_SYSTEMS
+from pyclts.models import is_valid_sound
+
+
+def register(parser):
+    parser.add_argument("dataset", help="the dataset")
+
+
+def run(args):
+    if not LINGPY:
+        raise ParserError('lingpy must be installed to run this command!')
+
+    def writer(*comps):
+        return UnicodeWriter(args.repos.path('pkg', *comps), delimiter='\t')
+
+    bipa = args.repos.bipa
+    rows = args.repos.get_source(args.dataset)
+    graphemes = {}
+    errors = []
+    for i, row in enumerate(rows):
+        
+        if row['GRAPHEME'] in graphemes:
+            if row['BIPA'] == graphemes[row['GRAPHEME']]:
+                args.log.info('duplicate grapheme in the data: {0}'.format(row['GRAPHEME']))
+            else:
+                args.log.warn('duplicate grapheme «{0}» has BIPA «{1}» and «{2}»'.format(
+                    row['GRAPHEME'],
+                    row['BIPA'],
+                    graphemes[row['GRAPHEME']]
+                    ))
+                errors += [(i+1, row['BIPA'], row['GRAPHEME'])]
+
+        graphemes[row['GRAPHEME']] = row['BIPA']
+        explicit = False
+        if not row['BIPA']:
+            bipa_sound = bipa[row['GRAPHEME']]
+        elif row['BIPA'] == '<NA>':
+            bipa_sound = '<NA>'
+            explicit = True
+        else:
+            bipa_sound = bipa[row['BIPA']]
+            explicit = True
+
+        if explicit and str(bipa_sound) == '<NA>':
+            pass
+        elif explicit and bipa_sound.type == 'marker':
+            pass
+        elif explicit and bipa_sound.type == 'unknownsound':
+            args.log.error('unknown sound encountered for BIPA «{0}» (Line {1})'.format(
+                row['BIPA'],
+                i+1))
+            errors += [(i+1, row['BIPA'], row['GRAPHEME'])]
+        elif explicit and not is_valid_sound(bipa_sound, bipa):
+            args.log.error('invalid BIPA «{0}» (Line {1})'.format(
+                row['BIPA'],
+                i+1))
+            errors += [(i+1, row['BIPA'], row['GRAPHEME'])]
+    if not errors:
+        args.log.info('No errors found in the data')
+    else:
+        args.log.info('Found {0} errors in the data.'.format(len(errors)))
