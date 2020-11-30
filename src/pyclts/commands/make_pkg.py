761b77cdc4f7ea8a0eb926f34659058ab570e13e
lingulist <mattis.list@lingpy.org>
2020-11-30 13:22:29 +0100

    remove lines which are not neededd

--- src/pyclts/commands/make_pkg.py
+++ src/pyclts/commands/make_pkg.py
@@ -34,30 +34,6 @@ def run(args):
         out = process_transcription_data(
                 rows, columns, src, uritemplate, bipa, args)
 
-        #out = [['BIPA_GRAPHEME', 'CLTS_NAME', 'GENERATED', 'EXPLICIT', 'GRAPHEME', 'URL'] + columns]
-        #graphemes = set()
-        #for row in rows:
-        #    if row['GRAPHEME'] in graphemes:
-        #        args.log.warn('skipping duplicate grapheme: {0}'.format(row['GRAPHEME']))
-        #        continue
-        #    graphemes.add(row['GRAPHEME'])
-        #    if not row['BIPA']:
-        #        bipa_sound = bipa[row['GRAPHEME']]
-        #        explicit = ''
-        #    else:
-        #        bipa_sound = bipa[row['BIPA']]
-        #        explicit = '+'
-        #    generated = '+' if bipa_sound.generated else ''
-        #    if is_valid_sound(bipa_sound, bipa):
-        #        bipa_grapheme = bipa_sound.s
-        #        bipa_name = bipa_sound.name
-        #    else:
-        #        bipa_grapheme, bipa_name = '<NA>', '<NA>'
-        #    url = uritemplate.expand(**row) if uritemplate else row.get('URL', '')
-        #    out.append(
-        #        [bipa_grapheme, bipa_name, generated, explicit, row['GRAPHEME'],
-        #         url] + [
-        #            row.get(c, '') for c in columns])
         found = len([o for o in out if o[0] != '<NA>'])
         args.log.info('... {0} of {1} graphemes found ({2:.0f}%)'.format(
             found, len(out), found / len(out) * 100))
