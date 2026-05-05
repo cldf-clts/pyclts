e9e28cd377cc2cfe3821ba88c10443c2f1f1db60
Robert Forkel <xrotwang@googlemail.com>
2026-05-05 18:18:46 +0200

    bump version for development

--- src/pyclts/__init__.py
+++ src/pyclts/__init__.py
@@ -4,4 +4,4 @@ Programmatic access to CLTS data.
 from pyclts.datatypes import TranscriptionSystem, TranscriptionData, SoundClasses  # noqa: F401
 from pyclts.api import CLTS  # noqa: F401
 
-__version__ = '4.0.0'
+__version__ = '4.0.1.dev0'
